38b1533cf1112a6fce4560b06ac9d61381757c28
keastar <keastar1986@gmail.com>
2023-11-26 20:01:04 +0200

    done 3HW TypeSCRIPT!
    done 3HW TypeSCRIPT!

--- src/4.ts
+++ src/4.ts
@@ -1,6 +1,57 @@
+class Key {
+    private signature: number;
+    
+    constructor() {
+        this.signature = Math.random() * 10;
+        // let key = this.signature;
+    }
+
+    getSignature() {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+};
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    private tenants: Person[] = [];
+
+    comeIn(person: Person) {
+        if (this.door === true) {
+            this.tenants.push(person);
+            console.log('Вы вошли в дом!');
+        } else {
+            console.log('Двери закрыты! Войти невозможно!');
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+};
+
+class myHouse extends House {
+
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('Двери открыты!')
+        } else {
+            console.log('Ключ не подходит! Двери закрыты!')
+        }
+    }
+};
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const house = new myHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
